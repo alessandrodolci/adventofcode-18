582a5eb81b0b5df9d80f8a2a1e6dd2ef089e00e2
Alessandro Dolci <alessandro.rn@gmail.com>
2018-12-05 09:14:22 +0100

    Completed solution for day 4 part two

--- day4/strategy_2.py
+++ day4/strategy_2.py
@@ -4,3 +4,16 @@ common.order_input("input.txt", "input_chronological.txt")
 
 sleep_hours = common.get_sleep_hours("input_chronological.txt")
 
+most_slept_minute = -1
+asleep_guard = None
+times_slept = 0
+for guard in sleep_hours:
+    for i in range (60):
+        if sleep_hours[guard][i] > times_slept:
+            most_slept_minute = i
+            asleep_guard = guard[1:]
+            times_slept = sleep_hours[guard][i]
+
+print("Guard that is most frequently asleep on the same minute: " + asleep_guard
+    + "\nMost slept minute: " + str(most_slept_minute))
+print("Result: " + str(int(asleep_guard) * most_slept_minute))
