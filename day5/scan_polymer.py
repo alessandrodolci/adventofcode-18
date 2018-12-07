2a135554c803484ed46171f9db7535ad52c846ea
Alessandro Dolci <alessandro.rn@gmail.com>
2018-12-07 19:49:05 +0100

    Completed solution for day 5 part one

--- day5/scan_polymer.py
+++ day5/scan_polymer.py
@@ -2,12 +2,7 @@ last_read = ''
 with open("input.txt", "r") as input_file:
     with open("temp.txt", "w") as output_file:
         for line in input_file:
-            for char in line:
-                if char != last_read.swapcase():
-                    output_file.write("%s" %last_read)
-                    last_read = char
-                else:
-                    last_read = ''
+            output_file.write(line)
 
 modified = True
 while modified == True:
@@ -20,7 +15,21 @@ while modified == True:
                     if char != last_read.swapcase():
                         output_file.write("%s" %last_read)
                         last_read = char
+                    else:
+                        print(last_read + ' ' + char)
+                        last_read = ''
                         if modified == False:
                             modified = True
-                    else:
-                        last_read = ''
+            output_file.write("%s" %last_read)
+    with open("output.txt", "r") as temp_input_file:
+        with open("temp.txt", "w") as temp_output_file:
+            for line in temp_input_file:
+                temp_output_file.write(line)
+
+units = 0
+with open("output.txt", "r") as input_file:
+    for line in input_file:
+        for char in line:
+            units += 1
+
+print("Number of remaining units: " + str(units))
