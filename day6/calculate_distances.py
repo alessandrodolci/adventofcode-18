c90009abc85caa72bd0143eefdb05653bbbe0e0c
Alessandro Dolci <alessandro.rn@gmail.com>
2018-12-10 18:45:50 +0100

    Continued solution for day 6 part one

--- day6/calculate_distances.py
+++ day6/calculate_distances.py
@@ -1,6 +1,7 @@
+import sys
 
 def calculate_distance(a, b):
-    return (b[0] - a[0]) + (b[1] - a[1])
+    return abs((b[0] - a[0]) + (b[1] - a[1]))
 
 coordinates = []
 total_area_edge_length = -1
@@ -15,9 +16,20 @@ with open("input.txt", "r") as input_file:
         if highest_coordinate > total_area_edge_length:
             total_area_edge_length = highest_coordinate
 
-distances = []
+distances = [['' for i in range(total_area_edge_length)] for i in range(total_area_edge_length)]
 for i in range(total_area_edge_length):
     for j in range(total_area_edge_length):
-        for coordinate in coordinates:
-            area_sizes.append(calculate_distance((i, j), coordinate))
-            distances
+        best_distance = sys.maxsize
+        for index, coordinate in enumerate(coordinates):
+            distance = calculate_distance((i, j), coordinate)
+            if distance < best_distance:
+                best_distance = distance
+                distances[i][j] = index
+
+best_area_size = -1
+coordinates_areas = {}
+for distance in distances:
+    for coordinate in distance:
+        coordinates_areas[str(coordinate)] = coordinates_areas.get(str(coordinate), 0) + 1
+
+print("Size of the largest area: " + str(max(coordinates_areas.values())))
